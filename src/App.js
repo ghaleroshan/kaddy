baebad51dde6cd314f5fd80328728a7f5edb4514
ghaleroshan <ghaleroshan@gmail.com>
2020-08-20 22:12:48 +1000

    Minor Improvements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
+import { auth } from "./config";
+import { InputForm } from "./components/form";
 import { ImageList } from "./components/images";
+import { CustomPagination } from "./common/constant/paginate";
+import { Spinner } from "./common/Spinner";
 
 export const App = () => {
+  const [data, setData] = useState({});
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState("hello");
+  const [counter, setCounter] = useState(1);
+
+  const getImages = async (query) => {
+    const URL = `https://api.unsplash.com/search/photos/?page=${counter}&per_page=12&query=${query}&orientation=landscape&client_id=${auth.client_id}`;
+    try {
+      const res = await Axios.get(URL);
+      setData(res.data);
+      setImages(res.data.results);
+    } catch (err) {
+      console.log("Error fetching images", err);
+    }
+  };
+
+  useEffect(() => {
+    getImages(query);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [counter]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setQuery(e.target.value);
+    getImages(query);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    getImages(query);
+  };
+
+  console.log(images);
   return (
     <div className="App">
-      <ImageList />
+      <InputForm handleChange={handleChange} handleClick={handleClick} />
+
+      {images && <ImageList images={images} />}
+
+      <CustomPagination data={data} setCounter={setCounter} />
     </div>
   );
 };
--- src/components/images/index.jsx
+++ src/components/images/index.jsx
@@ -1,11 +1,6 @@
-import React, { useState, useEffect } from "react";
-import Axios from "axios";
+import React from "react";
 import styled from "styled-components";
 import { asRem } from "../../helper";
-import { auth } from "../../config";
-import { InputForm } from "../form";
-import { Spinner } from "../../common/Spinner";
-import { CustomPagination } from "../../common/constant/paginate";
 
 const Container = styled.div`
   display: flex;
@@ -47,53 +42,14 @@ const TitleWrapper = styled.div`
   box-shadow: ${asRem(2)} ${asRem(2)} ${asRem(6)} rgba(0, 0, 0, 0.5);
 `;
 
-export const ImageList = () => {
-  const [data, setData] = useState({});
-  const [images, setImages] = useState([]);
-  const [query, setQuery] = useState("hello");
-  const [counter, setCounter] = useState(1);
-
-  const getImages = async (query) => {
-    const URL = `https://api.unsplash.com/search/photos/?page=${counter}&per_page=12&query=${query}&orientation=landscape&client_id=${auth.client_id}`;
-    Axios.get(URL)
-      .then((data) => {
-        setData(data.data);
-        setImages(data.data.results);
-      })
-      .catch((err) => console.log("Error fetching images", err));
-  };
-
-  console.log(data);
-  useEffect(() => {
-    getImages(query);
-  }, [counter, query]);
-
-  const handleChange = (e) => {
-    e.preventDefault();
-    setQuery(e.target.value);
-    getImages(query);
-  };
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    getImages(query);
-  };
-
+export const ImageList = ({ images }) => {
   return (
     <>
-      <InputForm handleChange={handleChange} handleClick={handleClick} />
-
       <Container>
-        {!images.length ? (
-          <Spinner />
-        ) : (
+        {images &&
           images.map((img) => (
-            <ImageWrapper>
-              <Img
-                key={img.id}
-                src={img.urls.regular}
-                alt={img.alt_description}
-              />
+            <ImageWrapper key={img.id}>
+              <Img src={img.urls.regular} alt={img.alt_description} />
               <TitleWrapper>
                 <span>
                   Photo By: <a href={img.user.links.self}> {img.user.name}</a>
@@ -103,10 +59,8 @@ export const ImageList = () => {
                 </span>
               </TitleWrapper>
             </ImageWrapper>
-          ))
-        )}
+          ))}
       </Container>
-      <CustomPagination data={data} setCounter={setCounter} />
     </>
   );
 };
